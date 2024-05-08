68e955cec23abf3d8ad793457afc8af7ade306ee
unwontedsponn <benpaulspooner@gmail.com>
2024-05-08 11:14:24 +0100

    Updated Header with different links

--- app/components/Header.js
+++ app/components/Header.js
@@ -12,13 +12,13 @@ const Header = () => {
         <nav>
           <ul className="flex items-center space-x-8">
             <li><Link href="/about" className="text-gray-600 hover:text-gray-900">About</Link></li>
-            <li><Link href="/speakers" className="text-gray-600 hover:text-gray-900">Speakers</Link></li>
-            <li><Link href="/agenda" className="text-gray-600 hover:text-gray-900">Agenda</Link></li>
+            <li><Link href="/artists" className="text-gray-600 hover:text-gray-900">Artists</Link></li>
             <li><Link href="/faqs" className="text-gray-600 hover:text-gray-900">FAQs</Link></li>
             <li><Link href="/contact" className="text-gray-600 hover:text-gray-900">Contact</Link></li>
             <li>
+
               {/* Buy Tickets Button */}
-              <Link href="/tickets" className="bg-black text-white px-4 py-2 rounded hover:bg-gray-800">BUY TICKETS</Link>
+              <a href="https://open.spotify.com/show/7Hg8aL7xyELS2CJ9TKYykC?si=eb671e91da174363" className="bg-black text-white px-4 py-2 rounded hover:bg-gray-800" target="_blank" rel="noopener noreferrer">LISTEN TO EPISODES</a>
             </li>
           </ul>
         </nav>
@@ -28,3 +28,10 @@ const Header = () => {
 };
 
 export default Header;
+
+
+
+// Old links
+{/* <li><Link href="/speakers" className="text-gray-600 hover:text-gray-900">Speakers</Link></li>
+<li><Link href="/agenda" className="text-gray-600 hover:text-gray-900">Agenda</Link></li> */}
+{/* <Link href="/tickets" className="bg-black text-white px-4 py-2 rounded hover:bg-gray-800">BUY TICKETS</Link> */}
